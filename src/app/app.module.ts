01c0aa1a3ee2ba9877ed1f209beb7929d4647816
akshayblevel <akshayblevel@gmail.com>
2021-08-22 08:27:40 +0530

    5 files modified, 4 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/employee-component/employee.ts
@@ -0,0 +1,7 @@
+export interface IEmployee {
+  id: number;
+  code: string;
+  name: string;
+  salary: number;
+  starRating: number;
+}
